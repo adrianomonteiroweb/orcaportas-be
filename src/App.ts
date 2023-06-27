b8942852b7d4827d6848626bd60ca9fea20c6cc4
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2023-06-27 16:51:31 -0300

    new: Create the index.ts and UserRoutes.ts files for the user route

--- src/App.ts
+++ src/App.ts
@@ -1,17 +1,25 @@
 import express, { Express } from "express";
 
+import routes from "./routes";
+
 class App {
   private expressApp: Express;
 
   constructor() {
     this.expressApp = express();
     this.expressApp.use(express.json());
+
+    this.useRoutes();
   }
 
   public startServer(port: number): void {
     this.expressApp.listen(port, () => {
       console.log(`Server is running on ${port} port`);
     });
+  }
+
+  public useRoutes(): void {
+    this.expressApp.use(routes)
   }
 }
 
